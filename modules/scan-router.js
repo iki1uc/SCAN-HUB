3e4472220a49925a1ca54eeda050a95abb602a2c
iki1uc <entertrainer@web.de>
2026-07-09 06:49:19 +0200

    Update scan-router.js

--- modules/scan-router.js
+++ modules/scan-router.js
@@ -1,28 +1,40 @@
 import { SCAN_DOM } from "./scan-dom.js";
-import { SCAN_ERROR } from "./scan-error.js";
+import { SCAN_ARG } from "./scan-arg.js";
+import { SCAN_POSITION } from "./scan-position.js";
 import { SCAN_UI } from "./scan-ui.js";
+import { SCAN_ERROR } from "./scan-error.js";
 import { SCAN_PERFORMANCE } from "./scan-performance.js";
 import { SCAN_SEO } from "./scan-seo.js";
 import { SCAN_DOC } from "./scan-doc.js";
-import { SCAN_LIVE } from "./scan-live.js";
 import { SCAN_COUNT } from "./scan-count.js";
+import { SCAN_LIVE } from "./scan-live.js";
 
 export const SCAN_ROUTER = {
-    run(type) {
+    route(type, payload) {
+
         let result;
 
         switch(type) {
             case "dom": result = SCAN_DOM.run(); break;
-            case "error": result = SCAN_ERROR.run(); break;
+            case "arg": result = SCAN_ARG.run(payload); break;
+            case "pos": result = SCAN_POSITION.run(); break;
             case "ui": result = SCAN_UI.run(); break;
+            case "error": result = SCAN_ERROR.run(); break;
             case "perf": result = SCAN_PERFORMANCE.run(); break;
             case "seo": result = SCAN_SEO.run(); break;
-            case "doc": result = SCAN_DOC.capture(); break;
-            default: result = "Unbekannt";
+            case "doc": result = SCAN_DOC.run(); break;
+            default: result = { error: "Unbekannter Scan" };
         }
 
-        SCAN_LIVE.send(result);
+        // COUNT
         SCAN_COUNT.add(type, result);
+
+        // LIVE
+        SCAN_LIVE.send({
+            type,
+            result,
+            timestamp: Date.now()
+        });
 
         return result;
     }
