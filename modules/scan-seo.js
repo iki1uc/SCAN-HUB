9a9e153c64cba169837d5e034ca2e4c576afb35e
iki1uc <entertrainer@web.de>
2026-07-09 06:44:12 +0200

    Update scan-seo.js

--- modules/scan-seo.js
+++ modules/scan-seo.js
@@ -2,7 +2,7 @@ export const SCAN_SEO = {
     run() {
         return {
             title: document.title,
-            metaDescription: document.querySelector("meta[name='description']")?.content || "none",
+            description: document.querySelector("meta[name='description']")?.content || "none",
             h1: document.querySelector("h1")?.innerText || "none"
         };
     }
