f44c1f9dc5d0973d5af49c5743b14808810c409c
iki1uc <entertrainer@web.de>
2026-07-09 06:38:19 +0200

    Add SCAN_PERFORMANCE module for performance metrics

---
+++ modules/scan-performance.js
@@ -0,0 +1,8 @@
+export const SCAN_PERFORMANCE = {
+    run() {
+        return {
+            loadTime: performance.now(),
+            domSize: document.querySelectorAll('*').length
+        };
+    }
+};
