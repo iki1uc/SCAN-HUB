0add2c15b323cc4649256822a8d62dbb1aeb5aa4
iki1uc <entertrainer@web.de>
2026-07-09 06:30:36 +0200

    Refactor scan-arg.js to remove SCAN_ARG export
    Removed SCAN_ARG export and replaced with an object containing description and last_type.

--- modules/scan-arg.js
+++ modules/scan-arg.js
@@ -1,6 +1,4 @@
-export const SCAN_ARG = {
-    run(input) {
-        return { type: typeof input, value: input };
-    }
-};
-
+{
+    "description": "Argument Scan Daten",
+    "last_type": "none"
+}
