7737b6b0b6a4fb332d5785cdbd0a1a3bd8485899
iki1uc <entertrainer@web.de>
2026-07-09 06:37:51 +0200

    Add SCAN_UI module to count UI elements

---
+++ modules/scan-ui.js
@@ -0,0 +1,9 @@
+export const SCAN_UI = {
+    run() {
+        return {
+            buttons: document.querySelectorAll("button").length,
+            inputs: document.querySelectorAll("input").length,
+            links: document.querySelectorAll("a").length
+        };
+    }
+};
