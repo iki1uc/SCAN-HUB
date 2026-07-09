31002fd286ea19b66216a00338de380e00738745
iki1uc <entertrainer@web.de>
2026-07-09 06:32:27 +0200

    Update router.js

--- router.js
+++ router.js
@@ -1,1 +1,9 @@
+import { SCAN_ROUTER } from "./modules/scan-router.js";
+
+export const Router = {
+    route(type, payload) {
+        const result = SCAN_ROUTER.route(type, payload);
+        console.log("SCAN RESULT:", result);
+    }
+};
 
